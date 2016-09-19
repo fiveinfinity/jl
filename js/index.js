f40b8206d3f24ee6fe2f36bbf42799e200898858
fiveinfinity <jlachance1@gmail.com>
2016-09-19 11:11:01 -0500

    added projects, css, about

--- js/index.js
+++ js/index.js
@@ -1,6 +1,20 @@
 $(document).ready(function(){
 	$('.triangle').click(function(){
 		$(this).toggleClass('is-active');
-		$('.nav').toggleClass('is-active');
+		$('.links').toggleClass('is-active');
 	});
+
+  $('a').on('click', function(event) {
+
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        window.location.hash = hash;
+      });
+    } 
+  });
+
 });
